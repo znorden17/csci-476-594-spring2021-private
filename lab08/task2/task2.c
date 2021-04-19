5356f0b966ee00d81cdfe7d533a5764a2e9fead5
Zoe <znorden17@outlook.com>
2021-04-19 18:20:59 -0400

    added task3

--- lab08/task2/task2.c
+++ lab08/task2/task2.c
@@ -17,15 +17,25 @@ int main()
 	BIGNUM *m = BN_new(); // message private key - from cmd ln
 	BIGNUM *e = BN_new(); // e
 	BIGNUM *n = BN_new(); // n
+	BIGNUM *C = BN_new(); // encrypted text result
+	BIGNUM *M = BN_new();
 	BIGNUM *d = BN_new();
+	
+
 
 	BN_hex2bn(&n, "DCBFFE3E51F62E09CE7032E2677A78946A849DC4CDDE3A4D0CB81629242FB1A5");
 	BN_hex2bn(&e, "010001");
 	BN_hex2bn(&m, "4120746f702073656372657421");
-		
-	BN_mod_exp(d,m,e,n,ctx);
-
+	BN_hex2bn(&d,"74D806F9F3A62BAE331FFE3F0A68AFE35B3D2E4794148AACBC26AA381CD7D30D");	
+	
+	BN_mod_exp(C,m,e,n,ctx);
 	printBN("Encryption:",C);
+
+	BN_mod_exp(M,C,d,n,ctx);
+	printBN("Decrypted:",M);
+
+	
+
 
 	return 0;
 }
